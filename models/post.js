867ff8aafe261fc8b4866326eb5a4b220a86ab05
julie-beaucage <201386196@edu.clg.qc.ca>
2024-12-05 11:29:26 -0500

    ok

--- models/post.js
+++ models/post.js
@@ -1,6 +1,7 @@
 import Model from './model.js';
 import Repository from './repository.js';
 import UserModel from './user.js';
+import LikeModel from './like.js';
 
 export default class Post extends Model {
     constructor() {
@@ -18,9 +19,20 @@ export default class Post extends Model {
 
     bindExtraData(instance) {
         instance = super.bindExtraData(instance);
-
+        //PARTIE POUR LIKE DU POST
+        let likesRepository = new Repository(new LikeModel()).get();
+        let usersRepository = new Repository(new UserModel()).get(instance.Id)
+        let likes = likesRepository.findByFilter(like => like.PostId == instance)
+        let postsLikesTab= [];
+        for (let like of likes){
+            let user = usersRepository.get(like.UserId);
+            if(user) postsLikesTab.push({Name:user.Name, Avatar: user.Avatar});
+        };
+        instance.Likes = postsLikesTab;
+        
+         
+        //PARTIE POUR AUTHOR POST
         let author = new Repository(new UserModel()).get(instance.Author);
-        console.log(instance);
         instance.Author = { Id: author.Id, Name: author.Name, Avatar: author.Avatar };
         return instance;
     }
